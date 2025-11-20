d4376e16b115ba58a82477a4611611eb5634bbba
Fakhruddin Fu'ad <fakhruddinfuad29@gmail.com>
2025-11-20 23:43:04 +0700

    layouts bootstrap

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/cek', function () {
+    return view('layouts.app');
+});
+Route::get('/login', function () {
+    return view('auth.login');
+});
